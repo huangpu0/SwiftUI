509a627ccdae613dc7adda6cf7ff6eb4e9ad396a
Will.H <huangpu0205@163.com>
2024-12-19 11:54:54 +0800

    Group

---
+++ SwiftUIDemo/SwiftUIDemo/Layout/04_Group/K_04_Group.swift
@@ -0,0 +1,38 @@
+//
+//  K_04_Group.swift
+//  SwiftUIDemo
+//
+//  Created by ycwh on 2024/12/19.
+//
+
+import SwiftUI
+
+struct K_04_Group: View {
+    var body: some View {
+        
+        ScrollView{
+            
+            Group {
+                Text("Hello World !11")
+                Text("Hello World !22")
+                
+                Group {
+                    Text("Hello World !33")
+                    Text("Hello World !44")
+                }.background(.blue)
+                
+            }.background(.red)
+            
+//            Group {
+//                Text("Hello World !33")
+//                Text("Hello World !44")
+//            }.background(.blue)
+            
+        }
+        
+    }
+}
+
+#Preview {
+    K_04_Group()
+}
